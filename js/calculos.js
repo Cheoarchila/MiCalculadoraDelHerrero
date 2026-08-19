403103b307764d7792a235e678fb2866388e3a61
Cheoarchila <86863465+Cheoarchila@users.noreply.github.com>
2026-08-19 17:02:49 -0500

    Create calculos.js

---
+++ js/calculos.js
@@ -0,0 +1,26 @@
+function calcular() {
+
+    const medidaFinal = Number(document.getElementById("medidaFinal").value);
+    const canales = Number(document.getElementById("canales").value);
+    const profundidad = Number(document.getElementById("profundidad").value);
+    const bordes = Number(document.getElementById("bordes").value);
+    const espesor = Number(document.getElementById("espesor").value);
+
+    const anchoCanal = medidaFinal / canales;
+    const altoCanal = profundidad;
+
+    const desarrollo =
+        (bordes * 2) +
+        medidaFinal +
+        ((canales - 1) * profundidad) -
+        (canales * 4 * espesor);
+
+    document.getElementById("anchoCanal").textContent =
+        Math.round(anchoCanal);
+
+    document.getElementById("altoCanal").textContent =
+        Math.round(altoCanal);
+
+    document.getElementById("desarrollo").textContent =
+        Math.round(desarrollo);
+}
