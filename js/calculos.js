b9c9de1ea2d90cae7682a31804f381274e2050af
Cheoarchila <86863465+Cheoarchila@users.noreply.github.com>
2026-08-27 19:13:28 -0500

    Update calculos.js

--- js/calculos.js
+++ js/calculos.js
@@ -22,13 +22,13 @@ function calcular() {
     const desarrollo = (bordes * 2) + medidaFinal + ((canales - 1) * profundidad) - (canales * 4 * espesor) + ((canales - 1) * espesor);
     
     // Inyección de resultados en pantalla
-    document.getElementById("anchoCanal").textContent = Math.round(anchoCanal) + " mm";
-    document.getElementById("altoCanal").textContent = Math.round(altoCanal) + " mm";
-    document.getElementById("desarrollo").textContent = Math.round(desarrollo) + " mm";
+    document.getElementById("anchoCanal").textContent = Math.round(anchoCanal);
+    document.getElementById("altoCanal").textContent = Math.round(altoCanal);
+    document.getElementById("desarrollo").textContent = Math.round(desarrollo);
 
     // --- Ciclo de Generación de Marcas (Actualizado con Lógica Taller) ---
     let marca = bordes - espesor;
-    let listaMarcas = "1.-) " + Math.round(marca) + " mm<br>";
+    let listaMarcas = "1.-) " + Math.round(marca)<br>;
     let contador = 2;
 
     const altoReducido = profundidad - (2 * espesor); // Mantiene las paredes en 13 mm reales
