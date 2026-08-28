51be98928946f1be600452cbbcb06ee4d953758a
Cheoarchila <86863465+Cheoarchila@users.noreply.github.com>
2026-08-27 22:30:59 -0500

    Update calculos.js

--- js/calculos.js
+++ js/calculos.js
@@ -28,7 +28,7 @@ function calcular() {
 
     // --- Ciclo de Generación de Marcas (Actualizado con Lógica Taller) ---
     let marca = bordes - espesor;
-    let listaMarcas = "1.-) " + Math.round(marca) + "<br>";
+    let listaMarcas = "1.-) <span>" + Math.round(marca) + "</span><br>";
     let contador = 2;
 
     const altoReducido = profundidad - (2 * espesor); // Mantiene las paredes en 13 mm reales
